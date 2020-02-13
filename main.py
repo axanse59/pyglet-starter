4fdb6b5f65fa57c60a86a18bc2cd64eb591e0d6c
Alex <>
2020-02-13 14:33:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,48 +4,28 @@ win= pyglet.window.Window() # create the window
 # Create a sprite
 img= pyglet.image.load('sheet.png')
 smol_img = img.get_region(x=112, y=96, width=32, height=32)
+spr = pyglet.sprite.Sprite(smol_img, x = 200, y = 200)
+
+keys = pyglet.window.key.KeyStateHandler()
 
 def update(dt):
-  pass
+  win.push_handlers(keys)
+  if keys[pyglet.window.key.LEFT]:
+    spr.x -= 1
+  if keys[pyglet.window.key.RIGHT]:
+    spr.x += 1
+  if keys[pyglet.window.key.DOWN]:
+    spr.y -= 1
+  if keys[pyglet.window.key.UP]:
+    spr.y += 1
 
 # Start the event loop
 @win.event
 def on_draw():
     win.clear()
-    smol_img.blit(100, 150)
-
-# Create a sprite
-Get= pyglet.image.load('Overworld.png')
-smol_img = img.get_region(x=56, y=96, width=32, height=32)
-
-def update(dt):
-  pass
-
-# Start the event loop
-@win.event
-def on_draw():
-    win.clear()
-    smol_img.blit(100, 150)
-
-three= pyglet.image.load('cave')
-smol_img = img.get_region(x=0, y=380, width=32, height=32)
-
-def update(dt):
-  pass
-
-# Start the event loop
-@win.event
-def on_draw():
-    win.clear()
-    smol_img.blit(100, 150)
-
-# Create the text object
-text = pyglet.text.Label('Welcome', x = 100, y = 50)
-
-
-# inside the loop:
-def on_draw():
-  text.draw()
+    smol_img.blit(100,100)
+    spr.draw()
+    
 
 
 pyglet.clock.schedule(update) 
